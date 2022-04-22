5b5b861e98180d4d4add8291c27284b54ae49a15
Str4hex <103687361+str4hex@users.noreply.github.com>
2022-04-22 14:16:34 +0300

    Add files via upload
    ++add new logics

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
-from utils import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
+from utils import candidate_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", candidate_all=load_candidates_from_json())
+    return render_template("index.html", candidate_all=candidate_json)
 
 
 @app.route("/candidate/<int:id>")
 def candidate(id):
-    return render_template("single.html", candidate=get_candidate(id))
+    candidate = get_candidate(id)
+    return render_template("single.html", candidate=candidate)
+
 
 @app.route("/search/<candidate_name>")
 def search(candidate_name):
@@ -20,7 +22,7 @@ def search(candidate_name):
 
 @app.route("/skill/<skill_name>")
 def skill(skill_name):
-    return render_template("skill.html", skill=get_candidates_by_skill(skill_name), skill_names=skill_name)
+    return render_template("skill.html", skill=get_candidates_by_skill(skill_name), skill_name=skill_name)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,38 +1,28 @@
-from api_load import api
+import json
 
-candidate_api = api()
-
-
-def load_candidates_from_json():
-    return api()
+with open("candidates.json", "r", encoding="utf8") as candidate:
+    candidate_json = json.load(candidate)
 
 
 def get_candidate(candidate_id):
-    candidate = []
-    for candidates in range(len(candidate_api)):
-        if candidate_api[candidates]["id"] == candidate_id:
-            candidate.append(candidate_api[candidates])
-        elif candidate_id > len(candidate_api):
-            return "Данный пользователь не найден"
-    return candidate
+    for candidates in candidate_json:
+        if candidates["id"] == candidate_id:
+            return candidates
 
 
 def get_candidates_by_name(candidate_name):
     candidate = []
-    for candidates in candidate_api:
+    for candidates in candidate_json:
         list_name = candidates.get("name").split(" ")
         if candidate_name in list_name:
-            print(candidate_name)
             candidate.append(candidates)
     return candidate
 
 
 def get_candidates_by_skill(skill_name="NO"):
     candidate = []
-    for candidates in candidate_api:
-        list_skill = candidates.get("skills").lower().split(",")
-        if skill_name in list_skill:
+    for candidates in candidate_json:
+        list_skill = candidates.get("skills").lower().split(", ")
+        if skill_name.lower() in list_skill:
             candidate.append(candidates)
     return candidate
-
-
